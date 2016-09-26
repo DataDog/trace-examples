bf16695e6e26b8bc8499b7d34806fbb6d64994a7
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2016-09-26 10:21:24 +0200

    [django] makes model Python 2 compatible; add Greeting admin

--- python/django/hello/models.py
+++ python/django/hello/models.py
@@ -1,14 +1,20 @@
+from __future__ import unicode_literals
 
 import logging
 
-from django.db import models
-from django.db import connection
+from django.db import models, connection
+from django.utils.encoding import python_2_unicode_compatible
+
 
 log = logging.getLogger()
 
-# Create your models here.
+
+@python_2_unicode_compatible
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+    def __str__(self):
+        return str(self.when)
 
 
 def explicit_query_error():
@@ -16,8 +22,7 @@ def explicit_query_error():
     cursor = connection.cursor()
     return cursor.execute("select error from non_existant_table").fetchall()
 
+
 def explicit_query_pass():
     cursor = connection.cursor()
     cursor.execute("select 1 from hello_greeting")
-
-
