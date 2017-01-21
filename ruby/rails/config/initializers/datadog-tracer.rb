43f452d219967fdb838b3d88b3b50b63446c4ca8
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-01-21 20:14:36 +0100

    [rails] services as environment variables

--- ruby/rails/config/initializers/datadog-tracer.rb
+++ ruby/rails/config/initializers/datadog-tracer.rb
@@ -4,7 +4,8 @@ require 'ddtrace'
 Rails.configuration.datadog_trace = {
   auto_instrument: true,
   auto_instrument_redis: true,
-  default_service: 'rails3-demo',
-  default_database_service: 'mysql',
+  default_service: ENV['RAILS_SERVICE'],
+  default_database_service: ENV['RAILS_MYSQL_SERVICE'],
+  default_cache_service: ENV['RAILS_CACHE_SERVICE'],
   trace_agent_hostname: ENV['DATADOG_TRACER'] || 'localhost'
 }
