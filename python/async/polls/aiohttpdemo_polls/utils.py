f155cd9ecb501e0f689f191f86e2b16c35a6df18
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-02-01 14:39:00 +0100

    [aiohttp] full polls application with templates and database access

---
+++ python/async/polls/aiohttpdemo_polls/utils.py
@@ -0,0 +1,18 @@
+import trafaret as T
+
+primitive_ip_regexp = r'^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$'
+
+TRAFARET = T.Dict({
+    T.Key('postgres'):
+        T.Dict({
+            'database': T.String(),
+            'user': T.String(),
+            'password': T.String(),
+            'host': T.String(),
+            'port': T.Int(),
+            'minsize': T.Int(),
+            'maxsize': T.Int(),
+        }),
+    T.Key('host'): T.String(regex=primitive_ip_regexp),
+    T.Key('port'): T.Int(),
+})
