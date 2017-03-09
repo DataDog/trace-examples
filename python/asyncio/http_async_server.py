fb42714d8379e63f1f9526ab5d5d46bfe85c8a92
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-03-09 18:48:21 +0100

    [asyncio] using package version 0.6.0

--- python/asyncio/http_async_server.py
+++ python/asyncio/http_async_server.py
@@ -19,7 +19,7 @@ Pin.override(client, service='asyncio-redis-queue')
 
 
 async def get_redis_value():
-    with tracer.trace("async.cache"):
+    with tracer.trace('async.cache'):
         # we may not block here
         await asyncio.sleep(0.01)
         value = client.get('sync:key')
@@ -27,13 +27,13 @@ async def get_redis_value():
 
 
 async def delayed_job(parent_span):
-    with tracer.start_child_span("async.worker", parent_span, service="asyncio-workers") as span:
+    with tracer.start_child_span('async.worker', parent_span, service='asyncio-workers') as span:
         await asyncio.sleep(3)
 
 
 async def handle_request(reader, writer):
     # trace something
-    with tracer.trace("async.handler", service="asyncio-web") as span:
+    with tracer.trace('async.handler', service='asyncio-web') as span:
         # do something
         await asyncio.sleep(0.02)
         # in the meantime do something else
@@ -43,27 +43,27 @@ async def handle_request(reader, writer):
 
     # response
     start_response(writer)
-    writer.write(b"OK\r\n")
+    writer.write(b'OK\r\n')
     writer.close()
     await future
-    print("200: request handled")
+    print('200: request handled')
 
 
-def start_response(writer, content_type=b"text/html"):
-    writer.write(b"HTTP/1.0 200 NA\r\n")
-    writer.write(b"Content-Type: ")
+def start_response(writer, content_type=b'text/html'):
+    writer.write(b'HTTP/1.0 200 NA\r\n')
+    writer.write(b'Content-Type: ')
     writer.write(content_type)
-    writer.write(b"\r\n\r\n")
+    writer.write(b'\r\n\r\n')
 
 
 hostname = os.getenv('APP_HOSTNAME', '127.0.0.1')
 port = int(os.getenv('APP_PORT', '8000'))
 
-print("-- Starting the server --")
 loop = asyncio.get_event_loop()
 try:
     loop.create_task(asyncio.start_server(handle_request, hostname, port))
+    print('-- Starting the server --')
     loop.run_forever()
 finally:
     loop.close()
-    print("-- Server closed --")
+    print('-- Server closed --')
