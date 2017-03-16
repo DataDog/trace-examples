30522492b39a7c1eb205bde8b6e913e19eb5c632
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-03-16 19:55:12 +0100

    [go] base webserver

--- go/http_server/main.go
+++ go/http_server/main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// give the response
+	path := r.URL.Path[1:]
+	fmt.Fprintf(w, "Hi there, I love %s!", path)
+}
+
 func main() {
+	// welcome
+	fmt.Println("--- Starting web server on port 8000 ---")
+
+	// web server
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8000", nil)
 }
