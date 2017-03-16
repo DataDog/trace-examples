63312510e7f2c3c2ba4b520f70c2b04ce7b7ff66
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-03-16 20:17:11 +0100

    [go] application that spawns parallel spans

--- go/http_server/main.go
+++ go/http_server/main.go
@@ -2,13 +2,44 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
+	"sync"
+	"time"
+
+	"github.com/Datadog/dd-trace-go/tracer"
+)
+
+const (
+	GO_ROUTINES = 10
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	// give the response
+	// trace the endpoint
 	path := r.URL.Path[1:]
+	rootSpan := tracer.NewRootSpan("http.client.request", "go-parallel", "/")
+	rootSpan.SetMeta("http.url", path)
+
+	// give the response
 	fmt.Fprintf(w, "Hi there, I love %s!", path)
+
+	// wait for goroutine termination
+	var wg sync.WaitGroup
+
+	// spawn parallel goroutines
+	for i := 0; i < GO_ROUTINES; i++ {
+		wg.Add(1)
+		go func() {
+			span := tracer.NewChildSpan("http.goroutine", rootSpan)
+			rand := time.Duration(rand.Intn(100))
+			time.Sleep(rand * time.Millisecond)
+			span.Finish()
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	rootSpan.Finish()
 }
 
 func main() {
