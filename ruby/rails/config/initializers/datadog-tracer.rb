ae62c93d8aa53693b9aa74c2c299389d84528d7f
Emanuele Palazzetti <emanuele.palazzetti@datadoghq.com>
2017-04-14 01:02:51 +0200

    [rails] upgrade Rails settings

--- ruby/rails/config/initializers/datadog-tracer.rb
+++ ruby/rails/config/initializers/datadog-tracer.rb
@@ -4,8 +4,8 @@ require 'ddtrace'
 Rails.configuration.datadog_trace = {
   auto_instrument: true,
   auto_instrument_redis: true,
-  default_service: ENV['RAILS_SERVICE'],
-  default_database_service: ENV['RAILS_MYSQL_SERVICE'],
-  default_cache_service: ENV['RAILS_CACHE_SERVICE'],
+  default_service: ENV['RAILS_SERVICE'] || 'rails-local',
+  default_database_service: ENV['RAILS_MYSQL_SERVICE'] || 'db-local',
+  default_cache_service: ENV['RAILS_CACHE_SERVICE'] || 'cache-local',
   trace_agent_hostname: ENV['DATADOG_TRACER'] || 'localhost'
 }
