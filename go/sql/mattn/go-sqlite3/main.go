b53930505f4695f2491f2b32675d006c235613d3
Erich Ess <erich.ess@datadoghq.com>
2018-08-01 16:37:27 -0400

    go/sql: Updating to use trace context.

--- go/sql/mattn/go-sqlite3/main.go
+++ go/sql/mattn/go-sqlite3/main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
@@ -15,10 +16,11 @@ import (
 func main() {
 	tracer.Start(tracer.WithDebugMode(true))
 	defer tracer.Stop()
+
 	sqltrace.Register(
 		"sqlite",
 		&sqlite.SQLiteDriver{},
-		sqltrace.WithServiceName("sqlite-example"))
+		sqltrace.WithServiceName("sqlite-example2"))
 
 	os.Remove("./foo.db")
 	db, err := sqltrace.Open("sqlite", "./foo.db")
@@ -27,11 +29,19 @@ func main() {
 	}
 	defer db.Close()
 
+	// Create a root span, giving name, server and resource.
+	x, ctx := tracer.StartSpanFromContext(context.Background(), "my-query",
+		tracer.SpanType("db"),
+		tracer.ServiceName("sqlite-example2"),
+		tracer.ResourceName("initial-access"),
+	)
+	defer x.Finish()
+
 	sqlStmt := `
 	create table foo (id integer not null primary key, name text);
 	delete from foo;
 	`
-	_, err = db.Exec(sqlStmt)
+	_, err = db.ExecContext(ctx, sqlStmt)
 	if err != nil {
 		log.Printf("%q: %s\n", err, sqlStmt)
 		return
@@ -41,20 +51,20 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	stmt, err := tx.Prepare("insert into foo(id, name) values(?, ?)")
+	stmt, err := tx.PrepareContext(ctx, "insert into foo(id, name) values(?, ?)")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer stmt.Close()
 	for i := 0; i < 100; i++ {
-		_, err = stmt.Exec(i, fmt.Sprintf("Number: %03d", i))
+		_, err = stmt.ExecContext(ctx, i, fmt.Sprintf("Number: %03d", i))
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
 	tx.Commit()
 
-	rows, err := db.Query("select id, name from foo")
+	rows, err := db.QueryContext(ctx, "select id, name from foo")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -73,29 +83,29 @@ func main() {
 		log.Fatal(err)
 	}
 
-	stmt, err = db.Prepare("select name from foo where id = ?")
+	stmt, err = db.PrepareContext(ctx, "select name from foo where id = ?")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer stmt.Close()
 	var name string
-	err = stmt.QueryRow("3").Scan(&name)
+	err = stmt.QueryRowContext(ctx, "3").Scan(&name)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(name)
 
-	_, err = db.Exec("delete from foo")
+	_, err = db.ExecContext(ctx, "delete from foo")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	_, err = db.Exec("insert into foo(id, name) values(1, 'foo'), (2, 'bar'), (3, 'baz')")
+	_, err = db.ExecContext(ctx, "insert into foo(id, name) values(1, 'foo'), (2, 'bar'), (3, 'baz')")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	rows, err = db.Query("select id, name from foo")
+	rows, err = db.QueryContext(ctx, "select id, name from foo")
 	if err != nil {
 		log.Fatal(err)
 	}
