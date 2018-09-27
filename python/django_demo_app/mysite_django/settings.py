37fd1d4aa7cb4a4808d5390c5b793bf50098bd9a
Luca Abbati <luca.abbati@gmail.com>
2018-09-27 17:48:49 +0200

    [django] Updated WSGI configuration for django 2 demo app

--- python/django_demo_app/mysite_django/settings.py
+++ python/django_demo_app/mysite_django/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'mysite_django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '/tmp/django20.sqlite3'),
     }
 }
 
