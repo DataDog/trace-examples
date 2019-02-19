e74373fb6fab4a09f447d6091b4f8eaee886aad8
Luca Abbati <luca.abbati@gmail.com>
2019-02-19 15:31:35 +0100

    [php] Add graphQL example to Laravel example

---
+++ php/Laravel57/database/migrations/2019_02_19_133739_create_comments_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('post_id');
+            $table->string('reply');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
