bdafbf1e46613de0eb816e1dd9105fb9fbc4fe8f
Caleb Gilmour <caleb.gilmour@datadoghq.com>
2019-10-10 14:17:46 +0000

    Example of datadog tracing for a twirp service

---
+++ go/twirp/cmd/traced-client/main.go
@@ -0,0 +1,54 @@
+// Copyright 2018 Twitch Interactive, Inc.  All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may not
+// use this file except in compliance with the License. A copy of the License is
+// located at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// or in the "license" file accompanying this file. This file is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+	twirptrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/twitchtv/twirp"
+
+	"github.com/twitchtv/twirp"
+	"github.com/DataDog/trace-examples/go/twirp"
+)
+
+func main() {
+	tracer.Start()
+	defer tracer.Stop()
+
+	client := example.NewHaberdasherJSONClient("http://localhost:8080", twirptrace.WrapClient(&http.Client{}))
+
+	var (
+		hat *example.Hat
+		err error
+	)
+	for i := 0; i < 5; i++ {
+		hat, err = client.MakeHat(context.Background(), &example.Size{Inches: 12})
+		if err != nil {
+			if twerr, ok := err.(twirp.Error); ok {
+				if twerr.Meta("retryable") != "" {
+					// Log the error and go again.
+					log.Printf("got error %q, retrying", twerr)
+					continue
+				}
+			}
+			// This was some fatal error!
+			log.Fatal(err)
+		}
+	}
+	fmt.Printf("%+v", hat)
+}
---
+++ go/twirp/cmd/traced-server/main.go
@@ -0,0 +1,56 @@
+// Copyright 2018 Twitch Interactive, Inc.  All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may not
+// use this file except in compliance with the License. A copy of the License is
+// located at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// or in the "license" file accompanying this file. This file is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package main
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+	twirptrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/twitchtv/twirp"
+
+
+	"github.com/twitchtv/twirp"
+	"github.com/DataDog/trace-examples/go/twirp"
+	"github.com/twitchtv/twirp/hooks/statsd"
+)
+
+type randomHaberdasher struct{}
+
+func (h *randomHaberdasher) MakeHat(ctx context.Context, size *example.Size) (*example.Hat, error) {
+	if size.Inches <= 0 {
+		return nil, twirp.InvalidArgumentError("Inches", "I can't make a hat that small!")
+	}
+	colors := []string{"white", "black", "brown", "red", "blue"}
+	names := []string{"bowler", "baseball cap", "top hat", "derby"}
+	return &example.Hat{
+		Size:  size.Inches,
+		Color: colors[rand.Intn(len(colors))],
+		Name:  names[rand.Intn(len(names))],
+	}, nil
+}
+
+func main() {
+	tracer.Start()
+	hooks := twirp.ChainHooks(
+		statsd.NewStatsdServerHooks(LoggingStatter{ioutil.Discard}),
+		twirptrace.NewServerHooks(),
+	)
+	server := example.NewHaberdasherServer(&randomHaberdasher{}, hooks)
+	traced := twirptrace.WrapServer(server)
+	log.Fatal(http.ListenAndServe(":8080", traced))
+}
