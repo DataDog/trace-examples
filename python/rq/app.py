b8b6b7b29f260d6f95b0baa98ceee14fc242a150
Kyle <kyle@verhoog.ca>
2019-10-15 13:31:17 -0400

    [python/rq] add job fetching

--- python/rq/app.py
+++ python/rq/app.py
@@ -1,6 +1,7 @@
 from random import choice, random
 from time import sleep
 
+from ddtrace import tracer
 from redis import Redis
 from rq import Queue
 
@@ -10,17 +11,16 @@ from jobs import jobs
 redis_conn = Redis()
 q = Queue(connection=redis_conn)
 
-# print(job.result)
-
-
-# import time; time.sleep(2)
-# print(job.result)
-
 while True:
     job, rargs = choice(jobs)
     args = rargs()
 
-    print(f'submitting job {job.__name__} with args {args}')
-    job = q.enqueue(job, *args)
-    # job.delay(*args)
-    sleep(random())
+    with tracer.trace('main', service='rq-app') as span:
+        print(f'submitting job {job.__name__} with args {args}')
+        job = q.enqueue(job, *args, job_id=job.__name__)
+        with tracer.trace('meantime', service='rq-app'):
+            sleep(0.25)
+        # print(job.get_status())
+        with tracer.trace('handle results', service='rq-app'):
+            q.fetch_job(job.id)
+        sleep(random())
