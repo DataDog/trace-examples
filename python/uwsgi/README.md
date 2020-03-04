50683c8e77170e4052f246b2cb6b23a456a1054a
Mike Tarkington <tarkmike@gmail.com>
2020-03-04 15:14:44 -0500

    Starter for uwsgi example image

---
+++ python/uwsgi/README.md
@@ -0,0 +1,7 @@
+# uWSGI Example
+
+## Setup
+
+```shell
+DD_API_KEY=... docker-compose up --build
+```
---
+++ python/uwsgi/wsgi.py
@@ -0,0 +1,7 @@
+from ddtrace import tracer
+
+
+@tracer.wrap()
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return [b"Hello World"]
