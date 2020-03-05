10fd19af89a0fb46be8519fceec8c4e271b42a0a
Mike Tarkington <tarkmike@gmail.com>
2020-03-05 18:40:18 -0500

    Add logging via ddtrace-run startup and ini config, add instructions to readme, properly set service name via wrap(), test logic for error handling thread config

--- python/uwsgi/README.md
+++ python/uwsgi/README.md
@@ -2,6 +2,9 @@
 
 ## Setup
 
-```shell
-DD_API_KEY=... docker-compose up --build
+Clone this repository and from the `trace-examples/python/uwsgi` run the following to build the Docker image and start the example app with uWSGI.  **Note that debug logging has been enabled for the tracing library in the `docker-compose.yml` file and the agent log level is currently set to `info`**
+
 ```
+DD_API_KEY=<your_api_key> docker-compose up --build
+```
+Go to your local browser and visit `localhost:8080` to generate traces.
--- python/uwsgi/wsgi.py
+++ python/uwsgi/wsgi.py
@@ -1,7 +1,27 @@
-from ddtrace import tracer
+import logging
+import uwsgi
+from ddtrace import tracer#, internal
+
+log = logging.getLogger(__name__)
 
 
-@tracer.wrap()
+@tracer.wrap(service="uwsgi-example")
 def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
+    # if uwsgi.opt["enable-threads"] != True:
+    #     log.debug("enable-threads=true is required for ddtrace to run")
+    #     internal.writer.on_shutdown()
     return [b"Hello World"]
+
+
+
+# LOG.debug("enable-threads={}".format(uwsgi.opt["enable-threads"]))
+
+# Possibly something like this for the 
+# try:
+#     import uwsgi
+#     if uwsgi.opt["enable-threads"] != True:
+#         log.debug("enable-threads=true is required for ddtrace to run")
+#         tracer.internal.writer.on_shutdown()
+# except:
+#     pass
