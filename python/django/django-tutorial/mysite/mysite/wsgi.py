d292dfa05dc080e4fc6e537f78196baa4e73593f
Tahir H. Butt <tahir.butt@datadoghq.com>
2020-11-06 16:13:29 -0500

    add uwsgi to django-tutorial with warmup

--- python/django/django-tutorial/mysite/mysite/wsgi.py
+++ python/django/django-tutorial/mysite/mysite/wsgi.py
@@ -10,7 +10,23 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.utils.functional import SimpleLazyObject
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
+
+# Warm-up the django application instead of letting it lazy-load
+application(
+    {
+        "REQUEST_METHOD": "GET",
+        "SERVER_NAME": SimpleLazyObject(lambda: "localhost"),
+        "REMOTE_ADDR": "127.0.0.1",
+        "SERVER_PORT": 80,
+        "PATH_INFO": "/polls/",
+        "wsgi.input": b"",
+        "wsgi.multiprocess": True,
+    },
+    lambda x, y: None,
+)
