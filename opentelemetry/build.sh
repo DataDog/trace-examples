913c63f60a44c1906f25f730b6c3d38cc45e5bd7
ericmustin <mustin.eric@gmail.com>
2021-02-18 16:29:20 -0500

    [otel]:update env vars

--- opentelemetry/build.sh
+++ opentelemetry/build.sh
@@ -3,6 +3,6 @@
 minikube start
 eval $(minikube docker-env)
 
-docker build -t node-microservice ./node-microservice --no-cache
 docker build -t python-microservice ./python-microservice --no-cache
+docker build -t node-microservice ./node-microservice --no-cache
 docker build -t ruby-microservice ./ruby-microservice --no-cache
